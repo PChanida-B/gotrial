fe8e309f8b0e8508839e2aecebbd038b268a70fb
ChanidaPc <P.chanidaB@gmail.com>
2022-11-19 00:54:39 +0700

    Add initail project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/PChanida-B/gotrial
+
+go 1.19
---
+++ gotrial.go
@@ -0,0 +1,17 @@
+package gotrial
+
+type Test struct {
+	Test string
+}
+
+//git add .
+//git commit -m "Add initail project"
+//git push origin master
+//or
+//git push origin HEAD:master
+//git tag v0.0.1
+//git push --tag origin master
+
+func TestPrint() string {
+	return "Hello"
+}
